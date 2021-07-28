313c9d016f02a18d95eb2e8ee1f819274e1a8e29
Hamd Zulfiqar <hamd.zulfiqar@tintash.com>
2021-07-28 12:12:07 +0500

    Post CRUD completed
    WHAT: CRUD operation of Post Model
    
    WHY: Project Requirement
    
    IMPACT: MEDIUM

--- app/controllers/api/v1/posts.js
+++ app/controllers/api/v1/posts.js
@@ -4,6 +4,7 @@ const Op = Sequelize.Op;
 
 module.exports =  (router) => {
 
+  //Old get and post
   router.post('/', async (req, res) => {
     req.body.userId = req.user.id;
     const post = await db.Posts.create(req.body);
@@ -17,5 +18,50 @@ module.exports =  (router) => {
       }]
     });
     res.json({posts: posts});
+  });
+
+  //Update and Delete of CRUD by Hamd Zulfiqar
+  router.put('/:id', async (req, res) => {
+    //console.log(req.params.id);
+    try {
+      const post = await db.Posts.findOne({where: {id: req.params.id}});
+      //console.log(post);
+
+      if(!post){
+        res.statusCode = 404;
+        res.json({ success: false, message: "Post not found!" });
+      }
+      else{
+        const {title, body} = req.body;
+        post.title = title;
+        post.body = body;
+
+        post.save();
+        res.statusCode = 200;
+        res.json({success: true, message: "Post updated", data: post});
+      }
+    } catch (error) {
+      res.statusCode = 500;
+      res.json({success: false, message: error});
+    }
+  });
+
+  router.delete('/:id', async (req, res) => {
+    try {
+      const post = await db.Posts.findOne({where: {id: req.params.id}});
+
+      if(!post){
+        res.statusCode = 404;
+        res.json({ success: false, message: "Post not found!" });
+      }
+      else{
+        await post.destroy();
+        res.statusCode = 200;
+        res.json({success: true, message: "Post deleted!"});
+      }
+    } catch (error) {
+      res.statusCode = 500;
+      res.json({success: false, message: error});
+    }
   });
 };
